de512b101c6b4b43bdf1ba35917a78378ec7a56e
jpromanonet <programming@jpromano.net>
2020-02-13 15:52:05 -0300

    Front almost there

--- controllers/controller_createAccount.php
+++ controllers/controller_createAccount.php
@@ -1,5 +1,11 @@
 <?php
+	// Take the data from the previous POST
+	
+	$userName = $_POST['userName'];
+	$email = $_POST['email'];
+	$pass = $_POST['password'];
 
+	// Calls the connection file
 	include 'models/conn.php';
 
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
@@ -11,15 +17,18 @@
 	
 	// Query to check if the email already exist
 	$checkEmail = "SELECT * FROM users WHERE Email = '$_POST[email]' ";
+	$checkUserName = "SELECT * FROM users WHERE userName = '$_POST[userName]' ";
 
-	// Variable $result hold the connection data and the query
-	$result = $conn-> query($checkEmail);
+	// Variables $resultMail and $resultUserName hold the connection data and the queries
+	$resultMail = $conn-> query($checkEmail);
+	$resultUserName = $conn-> query($checkUserName);
 
-	// Variable $count hold the result of the query
-	$count = mysqli_num_rows($result);
+	// Variables $countMail and $countUserName hold the result of the queries 
+	$countMail = mysqli_num_rows($resultMail);
+	$countUserName = mysqli_num_rows($resultUserName);
 
-	// If count == 1 that means the email is already on the database
-	if ($count == 1) {
+	// If countMail or countUserName == 1 that means the email or user name is already on the database
+	if ($countMail == 1 || $countUserName == 1) {
 	echo "<div class='alert alert-warning mt-4' role='alert'>
 					<p>That email is already in our database.</p>
 					<p><a href='login.html'>Please login here</a></p>
@@ -30,20 +39,15 @@
 	If the email don't exist, the data from the form is sended to the
 	database and the account is created
 	*/
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$email = $_POST['email'];
-	$pass = $_POST['password'];
 	
 	// The password_hash() function convert the password in a hash before send it to the database
 	$passHash = password_hash($pass, PASSWORD_DEFAULT);
 	
 	// Query to send Name, Email and Password hash to the database
-	$query = "INSERT INTO users (firstName, lastName, Email, Password) VALUES ('$firstName','$lastName', '$email', '$passHash')";
+	$query = "INSERT INTO users (userName, Email, Password, active) VALUES ('$userName', '$email', '$passHash', 1)";
 
 	if (mysqli_query($conn, $query)) {
-		echo "<div class='alert alert-success mt-4' role='alert'><h3>Your account has been created.</h3>
-		<a class='btn btn-outline-primary' href='login.html' role='button'>Login</a></div>";		
+			include "../views/login.html";		
 		} else {
 			echo "Error: " . $query . "<br>" . mysqli_error($conn);
 		}	
